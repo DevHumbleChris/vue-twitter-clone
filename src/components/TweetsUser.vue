6678687911e023ee530bd47fca5ef237f8368ad4
DevHumbleChris <dev.humblechris254@gmail.com>
2022-12-29 22:38:06 +0300

    Saving changes

--- src/components/TweetsUser.vue
+++ src/components/TweetsUser.vue
@@ -1,74 +1,42 @@
 <script setup>
+import { collection, onSnapshot, orderBy, query } from "@firebase/firestore"
 import { ChatBubbleOvalLeftIcon, HeartIcon, ArrowUpTrayIcon, ArrowsUpDownIcon } from "@heroicons/vue/20/solid"
+import { computed } from 'vue'
+import { db } from "../firebaseConfig";
+
+const tweets = computed(() => {
+    const q = query(collection(db, 'tweets'), orderBy('timestamp'))
+    const tweets = []
+    onSnapshot(q, (querySnapshot) => {
+        querySnapshot.forEach(doc => {
+            tweets.push({...doc.data(), id: doc.id})
+        })
+    })
+    return tweets
+})
+
+console.log(tweets.value)
 </script>
 
 <template>
     <div>
-        <div class="border border-gray-300 p-2">
-            <div class="flex space-x-2">
-                <img src="https://the-coding-montana.vercel.app/_next/image?url=%2Fimages%2Fthe-coding-montana.png&w=1920&q=75"
-                    alt="user-logo" class="w-16 h-16 border border-gray-200 rounded-full" />
-                <div>
-                    <div class="flex space-x-2 items-center">
-                        <h1>the codeFather</h1>
-                        <div class="w-2 h-2 bg-gray-300 rounded-full"></div>
-                        <p class="text-gray-500">13hs ago</p>
+        <div v-if="tweets">
+            <div v-for="tweet in tweets" :key="tweet.id" class="border border-gray-300 p-2">
+                <div class="flex space-x-2">
+                    <img :src="tweet.user.photoURL"
+                        alt="user-logo" class="w-16 h-16 border border-gray-200 rounded-full" />
+                    <div>
+                        <h1>{{ tweet.user.name }}</h1>
+                        <p class="text-gray-600 my-2">{{ tweet.tweet}}</p>
                     </div>
-                    <p class="text-gray-600 my-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro fuga
-                        harum laboriosam nesciunt, obcaecati iusto consequatur quas non laudantium odit beatae
-                        voluptatibus ab.</p>
+                </div>
+                <div class="flex justify-evenly m-2">
+                    <ChatBubbleOvalLeftIcon class="w-6 h-6 text-[#1ca0f2]" />
+                    <HeartIcon class="w-6 h-6 text-[#1ca0f2]" />
+                    <ArrowsUpDownIcon class="w-6 h-6 text-[#1ca0f2]" />
+                    <ArrowUpTrayIcon class="w-6 h-6 text-[#1ca0f2]" />
                 </div>
             </div>
-            <div class="flex justify-evenly m-2">
-                <ChatBubbleOvalLeftIcon class="w-6 h-6 text-[#1ca0f2]" />
-                <HeartIcon class="w-6 h-6 text-[#1ca0f2]" />
-                <ArrowsUpDownIcon class="w-6 h-6 text-[#1ca0f2]" />
-                <ArrowUpTrayIcon class="w-6 h-6 text-[#1ca0f2]" />
-            </div>
-        </div>
-        <div class="border border-gray-300 p-2">
-            <div class="flex space-x-2">
-                <img src="https://the-coding-montana.vercel.app/_next/image?url=%2Fimages%2Fthe-coding-montana.png&w=1920&q=75"
-                    alt="user-logo" class="w-16 h-16 border border-gray-200 rounded-full" />
-                <div>
-                    <div class="flex space-x-2 items-center">
-                        <h1>the codeFather</h1>
-                        <div class="w-2 h-2 bg-gray-300 rounded-full"></div>
-                        <p class="text-gray-500">13hs ago</p>
-                    </div>
-                    <p class="text-gray-600 my-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro fuga
-                        harum laboriosam nesciunt, obcaecati iusto consequatur quas non laudantium odit beatae
-                        voluptatibus ab.</p>
-                </div>
-            </div>
-            <div class="flex justify-evenly m-2">
-                <ChatBubbleOvalLeftIcon class="w-6 h-6 text-[#1ca0f2]" />
-                <HeartIcon class="w-6 h-6 text-[#1ca0f2]" />
-                <ArrowsUpDownIcon class="w-6 h-6 text-[#1ca0f2]" />
-                <ArrowUpTrayIcon class="w-6 h-6 text-[#1ca0f2]" />
-            </div>
-        </div>
-        <div class="border border-gray-300 p-2">
-            <div class="flex space-x-2">
-                <img src="https://the-coding-montana.vercel.app/_next/image?url=%2Fimages%2Fthe-coding-montana.png&w=1920&q=75"
-                    alt="user-logo" class="w-16 h-16 border border-gray-200 rounded-full" />
-                <div>
-                    <div class="flex space-x-2 items-center">
-                        <h1>the codeFather</h1>
-                        <div class="w-2 h-2 bg-gray-300 rounded-full"></div>
-                        <p class="text-gray-500">13hs ago</p>
-                    </div>
-                    <p class="text-gray-600 my-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro fuga
-                        harum laboriosam nesciunt, obcaecati iusto consequatur quas non laudantium odit beatae
-                        voluptatibus ab.</p>
-                </div>
-            </div>
-            <div class="flex justify-evenly m-2">
-                <ChatBubbleOvalLeftIcon class="w-6 h-6 text-[#1ca0f2]" />
-                <HeartIcon class="w-6 h-6 text-[#1ca0f2]" />
-                <ArrowsUpDownIcon class="w-6 h-6 text-[#1ca0f2]" />
-                <ArrowUpTrayIcon class="w-6 h-6 text-[#1ca0f2]" />
-            </div>
-        </div>
+        </div>f
     </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
-import { auth } from "./firebaseConfig";
+import { VueFire, VueFireAuth } from "vuefire";
+import { auth, firebaseApp } from "./firebaseConfig";
 
 import "./style.css";
 
@@ -15,6 +16,14 @@ auth.onAuthStateChanged(() => {
     app = createApp(App);
     app.use(createPinia());
     app.use(router);
+    app.use(VueFire, {
+      // imported above but could also just be created here
+      firebaseApp,
+      modules: [
+        // we will see other modules later on
+        VueFireAuth(),
+      ],
+    });
     app.mount("#app");
   }
 });
