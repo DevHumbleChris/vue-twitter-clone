8b3c1730b1bfc310ba7c5a984f8ea29b72d2dcd1
DevHumbleChris <dev.humblechris254@gmail.com>
2023-01-24 13:12:25 +0300

    saving changes

--- src/views/TweetPost.vue
+++ src/views/TweetPost.vue
@@ -1,5 +1,12 @@
 <script setup></script>
 
 <template>
-    <p>Tweet Post</p>
+    <section className="w-full scrollbar-hide overflow-scroll col-span-5 sm:col-span-4">
+        <div className="sticky top-0 p-2 flex space-x-2 items-center text-lg">
+            <router-link to="/home">
+                <ChevronLeftIcon className="w-6" />
+            </router-link>
+            <h1>Tweet</h1>
+        </div>
+    </section>
 </template>
