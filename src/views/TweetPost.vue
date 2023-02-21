f5b31415967d43e38500f131850fc26688b2ca6f
DevHumbleChris <dev.humblechris254@gmail.com>
2023-02-21 15:32:53 +0300

    successfully implemented the timeOfUpdate functionality

--- src/views/TweetPost.vue
+++ src/views/TweetPost.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ArrowsUpDownIcon, ChatBubbleOvalLeftIcon, ChevronLeftIcon, HeartIcon } from '@heroicons/vue/24/outline'
+import { ArrowsUpDownIcon, ChatBubbleOvalLeftIcon, ChevronLeftIcon, HeartIcon, PhotoIcon, XMarkIcon } from '@heroicons/vue/24/outline'
 import { RouterLink } from 'vue-router'
 import { watchEffect, ref, computed } from 'vue'
 import { useRoute } from 'vue-router'
@@ -9,162 +9,212 @@ import { HeartIcon as HeartIconFilled, ArrowsUpDownIcon as ArrowsUpDownIconFille
 import { format } from 'timeago.js';
 
 const user = computed(() => {
-    return auth.currentUser
+  return auth.currentUser
 })
 
 const route = useRoute()
+const tweetReply = ref('')
+const selectedFile = ref(null)
 
 const tweetID = computed(() => {
-    return route?.params?.tweetID
+  return route?.params?.tweetID
 })
 const tagName = computed(() => {
-    return route?.params?.tagName
+  return route?.params?.tagName
 })
 const tweet = ref(null)
 
 watchEffect(() => {
-    const unsub = onSnapshot(doc(db, "tweets", tweetID.value), (doc) => {
-        tweet.value = { ...doc.data(), id: doc.id }
-    });
+  const unsub = onSnapshot(doc(db, "tweets", tweetID.value), (doc) => {
+    tweet.value = { ...doc.data(), id: doc.id }
+  });
   return () => unsub()
 })
 
 const comments = ref([])
 watchEffect(() => {
-    const unsub = onSnapshot(collection(db, "tweets", tweetID.value, "comments"), (querySnapshot) => {
-        const myComments = []
-        querySnapshot.forEach(doc => {
-            myComments?.push({ ...doc.data(), id: doc.id })
-        })
-        comments.value = myComments
+  const unsub = onSnapshot(collection(db, "tweets", tweetID.value, "comments"), (querySnapshot) => {
+    const myComments = []
+    querySnapshot.forEach(doc => {
+      myComments?.push({ ...doc.data(), id: doc.id })
     })
-    return () => unsub()
+    comments.value = myComments
+  })
+  return () => unsub()
 })
 
 const likes = ref([])
 watchEffect(() => {
-    const unsub = onSnapshot(collection(db, "tweets", tweetID.value, "likes"), (querySnapshot) => {
-        const userLikes = []
-        querySnapshot.forEach(doc => {
-            userLikes?.push({ ...doc.data(), id: doc.id })
-        })
-        likes.value = userLikes
+  const unsub = onSnapshot(collection(db, "tweets", tweetID.value, "likes"), (querySnapshot) => {
+    const userLikes = []
+    querySnapshot.forEach(doc => {
+      userLikes?.push({ ...doc.data(), id: doc.id })
     })
-    return () => unsub()
+    likes.value = userLikes
+  })
+  return () => unsub()
 })
 
 const retweets = ref([])
 watchEffect(() => {
-    const unsub = onSnapshot(collection(db, "tweets", tweetID.value, "retweets"), (querySnapshot) => {
-        let userRetweets = []
-        querySnapshot.forEach(doc => {
-            userRetweets?.push({ ...doc.data(), id: doc.id })
-        })
-        retweets.value = userRetweets
+  const unsub = onSnapshot(collection(db, "tweets", tweetID.value, "retweets"), (querySnapshot) => {
+    let userRetweets = []
+    querySnapshot.forEach(doc => {
+      userRetweets?.push({ ...doc.data(), id: doc.id })
     })
-    return () => unsub()
+    retweets.value = userRetweets
+  })
+  return () => unsub()
 })
 
 const liked = ref(null)
 watchEffect(() => {
-    const isLiked = likes?.value.filter((like) => like?.id === user?.value.uid);
-    if (isLiked?.length > 0) {
-        liked.value = true
-    } else {
-        liked.value = false
-    }
+  const isLiked = likes?.value.filter((like) => like?.id === user?.value.uid);
+  if (isLiked?.length > 0) {
+    liked.value = true
+  } else {
+    liked.value = false
+  }
 })
 
 const isRetweeted = ref(null)
 watchEffect(() => {
-    const isRetweet = retweets?.value.filter((retweet) => retweet?.id === user?.value.uid);
-    if (isRetweet?.length > 0) {
-        isRetweeted.value = true
-    } else {
-        isRetweeted.value = false
-    }
+  const isRetweet = retweets?.value.filter((retweet) => retweet?.id === user?.value.uid);
+  if (isRetweet?.length > 0) {
+    isRetweeted.value = true
+  } else {
+    isRetweeted.value = false
+  }
 })
 
 const timeAgo = computed(() => {
-  let tweetTime = '1 min'
+  let tweetTime = ''
   if (tweet.value) {
     tweetTime = format(tweet?.value.timestamp?.toDate())
   }
   return tweetTime
 })
+
+const commentOnPost = () => {
+
+}
+
+const timeOfUpdate = (time) => {
+  let updateTime = ''
+  updateTime = format(time?.toDate())
+  return updateTime
+}
 </script>
 
 <template>
-    <section className="w-full scrollbar-hide overflow-scroll col-span-5 sm:col-span-4">
-        <div className="sticky top-0 p-2 flex space-x-2 items-center text-lg">
-            <RouterLink to="/home">
-                <ChevronLeftIcon className="w-6" />
-            </RouterLink>
-            <h1>Tweet</h1>
-        </div>
-        <div class="border-b border-gray-300">
-            <div class="p-3">
-              <div>
-                <div class="flex space-x-2">
-                  <img
-                    :src="tweet?.user?.photoURL"
-                    :alt="tagName"
-                    className="h-11 w-11 rounded-full"
-                  />
-                  <div>
-                    <div class="flex items-center space-x-2">
-                      <h4 class="font-bold text-[15px] sm:text-base">
-                        {{tweet?.user.name}}
-                      </h4>
-                      <div class="w-2 h-2 rounded-full bg-gray-400"></div>
-                      <div>{{ timeAgo }}</div>
-                    </div>
-                    <h5 class="text-[15px] sm:text-base">
-                      <span class="text-[#1ca0f2]">{{tagName}}</span>
-                    </h5>
-                  </div>
-                </div>
-                <p class="text-gray-600 my-2">{{tweet?.tweet}}</p>
-                  <div v-if="tweet?.image" class="my-2">
-                    <img
-                      :src="tweet?.image"
-                      :alt="tagName"
-                      class="object-contain rounded-xl"
-                    />
-                  </div>
+  <section className="w-full scrollbar-hide overflow-scroll col-span-5 sm:col-span-4">
+    <div className="sticky top-0 p-2 flex space-x-2 items-center text-lg">
+      <RouterLink to="/home">
+        <ChevronLeftIcon className="w-6" />
+      </RouterLink>
+      <h1>Tweet</h1>
+    </div>
+    <div class="border-b border-gray-300">
+      <div class="p-3">
+        <div>
+          <div class="flex space-x-2">
+            <img :src="tweet?.user?.photoURL" :alt="tagName" className="h-11 w-11 rounded-full" />
+            <div>
+              <div class="flex items-center space-x-2">
+                <h4 class="font-bold text-[15px] sm:text-base">
+                  {{ tweet?.user.name }}
+                </h4>
+                <div class="w-2 h-2 rounded-full bg-gray-400"></div>
+                <div>{{ timeAgo }}</div>
               </div>
-              <div class="text-[15px] my-2 sm:text-base flex items-center justify-between">
-                <div class="flex space-x-2">
-                  <p>{{comments.length}}</p>
-                  <p>Comments</p>
-                </div>
-                <div class="flex space-x-2">
-                  <p>{{likes.length}}</p>
-                  <p>Likes</p>
-                </div>
-                <div class="flex space-x-2">
-                  <p>{{retweets.length}}</p>
-                  <p>Retweets</p>
-                </div>
-              </div>
-              <div class="flex my-3 justify-between w-full">
-                <div class="flex items-center space-x-1">
-                  <ChatBubbleOvalLeftIcon class="w-6 h-6 text-[#1ca0f2]" />
-                </div>
-                <div
-                  class="flex items-center space-x-1 cursor-pointer"
-                >
-                <HeartIconFilled v-if="liked" class="text-[#f60100] w-6" />
-                <HeartIcon v-else class="w-6 h-6 text-[#1ca0f2]" />
-                </div>
-                <div
-                  class="flex items-center space-x-1 cursor-pointer"
-                >
-                <ArrowsUpDownIconFilled v-if="isRetweeted" class="w-6 h-6 text-green-700" />
-                <ArrowsUpDownIcon v-else class="w-6 h-6 text-[#1ca0f2]" />
-                </div>
-              </div>
+              <h5 class="text-[15px] sm:text-base">
+                <span class="text-[#1ca0f2]">{{ tagName }}</span>
+              </h5>
             </div>
           </div>
-    </section>
+          <p class="text-gray-600 my-2">{{ tweet?.tweet }}</p>
+          <div v-if="tweet?.image" class="my-2">
+            <img :src="tweet?.image" :alt="tagName" class="object-contain rounded-xl" />
+          </div>
+        </div>
+        <div class="text-[15px] my-2 sm:text-base flex items-center justify-between">
+          <div class="flex space-x-2">
+            <p>{{ comments.length }}</p>
+            <p>Comments</p>
+          </div>
+          <div class="flex space-x-2">
+            <p>{{ likes.length }}</p>
+            <p>Likes</p>
+          </div>
+          <div class="flex space-x-2">
+            <p>{{ retweets.length }}</p>
+            <p>Retweets</p>
+          </div>
+        </div>
+        <div class="flex my-3 justify-between w-full">
+          <div class="flex items-center space-x-1">
+            <ChatBubbleOvalLeftIcon class="w-6 h-6 text-[#1ca0f2]" />
+          </div>
+          <div class="flex items-center space-x-1 cursor-pointer">
+            <HeartIconFilled v-if="liked" class="text-[#f60100] w-6" />
+            <HeartIcon v-else class="w-6 h-6 text-[#1ca0f2]" />
+          </div>
+          <div class="flex items-center space-x-1 cursor-pointer">
+            <ArrowsUpDownIconFilled v-if="isRetweeted" class="w-6 h-6 text-green-700" />
+            <ArrowsUpDownIcon v-else class="w-6 h-6 text-[#1ca0f2]" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="border-b border-gray-300 p-2">
+      <div class="flex mt-4 space-x-3 w-full">
+        <img :src="user?.photoURL" :alt="user?.displayName" class="h-11 w-11 rounded-full" />
+        <form @submit.prevent="commentOnPost" class="flex-grow">
+          <textarea :placeholder="'Replying to ' + tagName" rows="2" v-model="tweetReply"
+            class="outline-none tracking-wide min-h-[80px] bg-transparent w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"></textarea>
+          <div v-if="selectedFile" class="relative my-2">
+            <div class="w-8 h-8 left-1 cursor-pointer">
+              <XMarkIcon class="text-black h-5" />
+            </div>
+            <img :src="selectedFile" :alt="user?.displayName" class="rounded-2xl max-h-80 object-contain mb-2" />
+          </div>
+          <div class="flex items-center justify-between">
+            <div class="flex items-center space-x-2">
+              <div class="flex items-center cursor-pointer">
+                <PhotoIcon class="w-8 text-[#1ca0f2]" />
+                <input type="file" hidden />
+              </div>
+            </div>
+            <button class="bg-[#1ca0f2] text-white p-2 my-2 rounded-2xl" disabled={!tweetReply} type="submit">
+              Tweet
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+    <div class="p-3">
+      <div class="relative">
+        <div v-for="comment in comments" :key="comment.id" class="flex space-x-2 my-4 border-b border-gray-300">
+          <img :src="comment?.user?.photoURL" :alt="comment?.user?.displayName" class="h-11 w-11 rounded-full" />
+          <div>
+            <div class="flex items-center space-x-2">
+              <h4 class="font-bold text-[15px] sm:text-base">
+                {{ comment?.user.name }}
+              </h4>
+              <div class="w-2 h-2 rounded-full bg-gray-400"></div>
+              <div>{{ timeOfUpdate(comment?.timestamp)}}</div>
+            </div>
+            <h5 class="text-[15px] sm:text-base">
+              Replying to
+              <span class="text-[#1ca0f2] mx-2">{{ tagName }}</span>
+            </h5>
+            <p class="text-gray-600 my-2">{{ comment?.comment }}</p>
+            <div v-if="comment?.image" className="my-2">
+              <img :src="comment?.image" :alt="comment?.id" class="object-contain rounded-xl" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
