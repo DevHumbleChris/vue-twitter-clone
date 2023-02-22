a8f8ae6688c4a74e0320dfd7c1cd65f28ddc5d59
DevHumbleChris <dev.humblechris254@gmail.com>
2023-02-22 10:25:54 +0300

    added page error 404 functionality

--- src/router/index.js
+++ src/router/index.js
@@ -22,21 +22,15 @@ const router = createRouter({
       ]
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
-      meta: {
-        requiresAuth: true,
-      },
-    },
-    {
       path: "/login",
       name: "login",
       component: () => import("../views/LoginView.vue"),
     },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: () => import('../views/NotFound.vue')
+    }
   ],
 });
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,22 @@
+<script setup>
+import SvgError404 from '@/components/SvgError404.vue'
+</script>
+
+<template>
+    <div class="text-center justify-center my-6 space-y-3">
+        <SvgError404 />
+        <div class=" tracking-widest space-y-2">
+            <p class="text-[#32475c] text-6xl">
+                <span>4 0 4</span>
+            </p>
+            <p class="text-[#40b27f] text-xl">
+                Sorry, We couldn't find what you are looking for!
+            </p>
+        </div>
+        <router-link to="/home">
+            <button class="text-white text-xl bg-[#32475c] p-3 rounded-md hover:shadow-md my-2">
+                Go back
+            </button>
+        </router-link>
+    </div>
+</template>
